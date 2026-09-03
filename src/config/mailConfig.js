e142ec7506379316b80ca346f3cb8ab49c6631e1
Madhumita15 <madhumita011das@gmail.com>
2026-09-03 21:34:27 +0530

    changes on mailconfig.js

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
+  pool: true,
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
